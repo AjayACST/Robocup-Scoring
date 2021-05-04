cfc8f627f547821129795dc1fb8640b17b972412
Leon Bowie <leon@bowie-co.nz>
2021-05-05 01:57:39 +1200

    Initial code base

---
+++ backend/app/app/schemas/__init__.py
@@ -0,0 +1,12 @@
+from .token import Token, TokenPayload
+from .msg import Msg
+from .user import User, UserCreate, UserInDB, UserUpdate
+
+__all__ = (
+    "Token",
+    "TokenPayload",
+    "User",
+    "UserCreate",
+    "UserInDB",
+    "UserUpdate",
+)
