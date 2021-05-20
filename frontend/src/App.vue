a64d01f83774bb13d2d9f1456176990b1c379b06
Leon Bowie <leon@bowie-co.nz>
2021-05-20 20:41:43 +1200

    Update to vuejs v3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,43 +1,27 @@
 <template>
-  <div id="app">
-    <v-app>
-      <v-content v-if="loggedIn===null">
-        <v-container fill-height>
-          <v-layout align-center justify-center>
-            <v-flex>
-              <div class="text-xs-center">
-                <div class="headline my-5">Loading...</div>
-                <v-progress-circular size="100" indeterminate color="primary"></v-progress-circular>
-              </div>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
-      <router-view v-else />
-      <NotificationsManager></NotificationsManager>
-    </v-app>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import NotificationsManager from '@/components/NotificationsManager.vue';
-import { readIsLoggedIn } from '@/store/main/getters';
-import { dispatchCheckLoggedIn } from '@/store/main/actions';
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-@Component({
+export default defineComponent({
+  name: 'App',
   components: {
-    NotificationsManager,
-  },
+    HelloWorld
+  }
 })
-export default class App extends Vue {
-
-  get loggedIn() {
-    return readIsLoggedIn(this.$store);
-  }
-
-  public async created() {
-    await dispatchCheckLoggedIn(this.$store);
-  }
-}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,4 @@
-import '@babel/polyfill';
-// Import Component hooks before component definitions
-import './component-hooks';
-import Vue from 'vue';
-import './plugins/vuetify';
-import './plugins/vee-validate';
-import App from './App.vue';
-import router from './router';
-import store from '@/store';
-import './registerServiceWorker';
-import 'vuetify/dist/vuetify.min.css';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+createApp(App).mount('#app')
